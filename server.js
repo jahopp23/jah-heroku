3fd97f47847f2df1b90f6b6ed12d88807e42bfd5
John A. Hopper <jahopp@gmail.com>
2017-09-13 01:45:52 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express(); 
+var path = require('path'); 
+
+app.use(express.static(__dirname + '/'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+app.get('/', function(req, res) { 
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+}); 
+
+app.listen(process.env.PORT || 3000);
+console.log('Running Port 3000');
+
+
+
